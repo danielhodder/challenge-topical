329114ac43e2a7658476c163078603c57aed48e8
Daniel Hodder <danielhodder@gmail.com>
2013-06-14 12:03:32 +1200

    Initial Rails 4 App shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChallengeTopical::Application.config.secret_key_base = '828eef1d159cd34f430b39c43c19a3fa07478f64adcd6a363ab18753f13c75b722dfa72fabf871f61d79f1e7f507c246b867ac3524d721eeaeac43fafc92dcbe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChallengeTopical::Application.config.session_store :cookie_store, key: '_challenge-topical_session'
